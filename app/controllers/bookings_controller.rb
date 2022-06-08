1345bbdbf2d92585904f870f6a00645d9bbc18c9
Samuel Coizet <messa57fr@gmail.com>
2022-06-08 16:01:33 +0200

    dates c bon

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,7 +4,11 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
+    @watch = Watch.find(@booking.watch_id)
+    @user = User.find(@booking.user_id)
+    @watch_price = total_booking
     authorize @booking
+
   end
 
   def create
@@ -18,6 +22,12 @@ class BookingsController < ApplicationController
       render :show
     end
     authorize @booking
+  end
+
+  def total_booking
+    @watch = Watch.find(@booking.watch_id)
+    @number_of_days = (@booking.end_date) - (@booking.start_date)
+    @total_price = (@watch.price * @number_of_days).to_i
   end
 
   private
